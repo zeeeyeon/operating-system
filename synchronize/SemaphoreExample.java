cfe225814617b3bbc55b7cc4527fc317f38172d0
zeeeyeon <zeeyeonleee@gmail.com>
2024-12-09 19:21:51 +0900

    semaphore test

---
+++ synchronize/SemaphoreExample.java
@@ -0,0 +1,30 @@
+package synchronize;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreExample {
+    // 동시에 3개의 스레드에 접근 가능
+    private final Semaphore semaphore = new Semaphore(3);
+
+    public void accessResource(int threadId) {
+        try {
+            // 자원 진입 시도
+            System.out.println("Thread: "  +  threadId + " try to access resource");
+
+            // 허가 획득 시도
+            semaphore.acquire();
+
+            // 자원 진입
+            System.out.println("Thread: "  +  threadId + " is accessing resource");
+
+            // 자원에 대한 작업을 수행하는 동안 sleep 으로 로그 보기
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            System.out.println("Thread: "  +  threadId + " released resource");
+            semaphore.release();
+        }
+
+    }
+}
---
+++ synchronize/SemaphoreExampleTest.java
@@ -0,0 +1,21 @@
+package synchronize;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreExampleTest {
+    public static void main(String[] args) throws InterruptedException {
+        final SemaphoreExample semaphoreExample = new SemaphoreExample();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            final int threadId = i;
+            executor.submit(() -> semaphoreExample.accessResource(threadId));
+        }
+
+        executor.shutdown();
+        boolean finished = executor.awaitTermination(20, TimeUnit.SECONDS);
+        assert finished;
+    }
+}
